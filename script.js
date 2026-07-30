2f5d939764f779c9630b32cf640f86b55986049a
ChrWink30-lang <ChrWink30@realschuleplus-mendig.org>
2026-07-30 21:37:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,126 @@
-let seiten=[];
+let tagebuch =
+JSON.parse(localStorage.getItem("tagebuch")) || [];
 
 
-function neueSeite(){
-
-let titel=prompt(
-"Seitentitel:"
-);
+let bild="";
 
 
-let text=prompt(
-"Was möchtest du schreiben?"
-);
+
+document
+.getElementById("bild")
+.addEventListener("change",function(){
+
+let datei=this.files[0];
+
+if(!datei)return;
 
 
-let seite={
+let reader=new FileReader();
 
-titel:titel,
 
-text:text,
+reader.onload=function(){
 
-datum:new Date().toLocaleDateString()
+bild=reader.result;
+
+
+let img=document.getElementById("vorschau");
+
+img.src=bild;
+img.style.display="block";
 
 };
 
 
-seiten.push(seite);
+reader.readAsDataURL(datei);
 
 
-speichern();
+});
 
 
-anzeigen();
-
-}
 
 
 
 function speichern(){
 
+
+let eintrag={
+
+titel:
+document.getElementById("titel").value,
+
+datum:
+document.getElementById("datum").value ||
+new Date().toLocaleDateString(),
+
+text:
+document.getElementById("text").value,
+
+bild:bild
+
+};
+
+
+tagebuch.push(eintrag);
+
+
+
 localStorage.setItem(
 "tagebuch",
-JSON.stringify(seiten)
+JSON.stringify(tagebuch)
 );
 
-}
-
-
-
-function laden(){
-
-let daten=
-localStorage.getItem("tagebuch");
-
-
-if(daten){
-
-seiten=JSON.parse(daten);
-
-}
 
 
 anzeigen();
 
+
 }
+
+
 
 
 
 function anzeigen(){
 
+
 let liste=
-document.getElementById(
-"seitenListe"
-);
+document.getElementById("seiten");
 
 
 liste.innerHTML="";
 
 
-seiten.forEach(
-(seite)=>{
+
+tagebuch.forEach((seite,index)=>{
 
 
-liste.innerHTML += `
+liste.innerHTML+=`
 
-<h3>
-${seite.titel}
-</h3>
+<div class="seite">
 
-<p>
-${seite.text}
-</p>
+<h3>${seite.titel}</h3>
 
-small>
-${seite.datum}
-</small>
+<p>${seite.datum}</p>
+
+<p>${seite.text}</p>
 
 
-<hr>
+${seite.bild ?
+`<img src="${seite.bild}">`
+:
+""}
 
+
+
+<button class="loeschen"
+onclick="loeschen(${index})">
+
+🗑 Löschen
+
+</button>
+
+
+</div>
 
 `;
 
@@ -108,5 +131,164 @@ ${seite.datum}
 
 
 
-laden();
 
+function loeschen(index){
+
+
+tagebuch.splice(index,1);
+
+
+localStorage.setItem(
+"tagebuch",
+JSON.stringify(tagebuch)
+);
+
+
+anzeigen();
+
+
+}
+
+
+
+
+
+async function pdfErstellen(){
+
+
+const {jsPDF}=window.jspdf;
+
+
+let pdf=
+new jsPDF();
+
+
+
+let y=20;
+
+
+
+pdf.setFontSize(22);
+
+pdf.text(
+"Mein Tagebuch",
+20,
+y
+);
+
+
+y+=20;
+
+
+
+for(let seite of tagebuch){
+
+
+
+if(y>250){
+
+pdf.addPage();
+
+y=20;
+
+}
+
+
+
+pdf.setFontSize(16);
+
+
+pdf.text(
+seite.titel || "Ohne Titel",
+20,
+y
+);
+
+
+y+=10;
+
+
+
+pdf.setFontSize(12);
+
+
+let text=
+pdf.splitTextToSize(
+seite.text,
+170
+);
+
+
+pdf.text(
+text,
+20,
+y
+);
+
+
+
+y+=text.length*7;
+
+
+
+pdf.text(
+seite.datum,
+20,
+y
+);
+
+
+
+y+=15;
+
+
+
+if(seite.bild){
+
+
+try{
+
+
+pdf.addImage(
+seite.bild,
+"JPEG",
+20,
+y,
+80,
+60
+);
+
+
+y+=70;
+
+
+}
+
+catch(e){
+
+console.log(
+"Bild konnte nicht eingefügt werden"
+);
+
+}
+
+
+}
+
+
+
+}
+
+
+
+pdf.save(
+"Mein-Tagebuch.pdf"
+);
+
+
+
+}
+
+
+
+anzeigen();
