93edbb2fc763c362bed2a864db6cdbb114e00fcd
ChrWink30-lang <ChrWink30@realschuleplus-mendig.org>
2026-07-30 21:33:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,112 @@
+let seiten=[];
+
+
+function neueSeite(){
+
+let titel=prompt(
+"Seitentitel:"
+);
+
+
+let text=prompt(
+"Was möchtest du schreiben?"
+);
+
+
+let seite={
+
+titel:titel,
+
+text:text,
+
+datum:new Date().toLocaleDateString()
+
+};
+
+
+seiten.push(seite);
+
+
+speichern();
+
+
+anzeigen();
+
+}
+
+
+
+function speichern(){
+
+localStorage.setItem(
+"tagebuch",
+JSON.stringify(seiten)
+);
+
+}
+
+
+
+function laden(){
+
+let daten=
+localStorage.getItem("tagebuch");
+
+
+if(daten){
+
+seiten=JSON.parse(daten);
+
+}
+
+
+anzeigen();
+
+}
+
+
+
+function anzeigen(){
+
+let liste=
+document.getElementById(
+"seitenListe"
+);
+
+
+liste.innerHTML="";
+
+
+seiten.forEach(
+(seite)=>{
+
+
+liste.innerHTML += `
+
+<h3>
+${seite.titel}
+</h3>
+
+<p>
+${seite.text}
+</p>
+
+small>
+${seite.datum}
+</small>
+
+
+<hr>
+
+
+`;
+
+});
+
+
+}
+
+
+
+laden();
 
